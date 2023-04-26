32a108efefd25bc6cc3a5884a8924769673f06e8
Hustra03 <erik.paulinder@gmail.com>
2023-04-26 10:31:16 +0200

    Added hard-coded calls from view to controller

--- view/View.java
+++ view/View.java
@@ -9,11 +9,41 @@ public class View {
 	public View(Controller controller) {
 		this.controller=controller;
 	}
-	/* Starts system operation to begin startSale
+	/* Starts system operation to start Sale
 	 * 
 	 */
 	public void StartSale()
 	{
 		controller.startSale();
+	}
+
+	/* Starts system operation to register an item
+	 * 
+	 */
+	public void registerItem()
+	{
+		controller.registerItem(0, 0);
+	}
+
+	/* Starts system operation to stop registering items
+	 * 
+	 */
+	public void endSale()
+	{
+		controller.endSale();
+	}
+
+	/* Starts system operation to get and apply discount to sale
+	 * 
+	 */
+	public void getDiscount()
+	{
+		controller.getDiscount(0);
+	}
+
+	public void recivePaymentAndSendSaleInfo()
+	{
+		controller.recivePayment(0);
+		controller.sendSaleInfo();
 	}
 }
