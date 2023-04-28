cbae8c35f9961c5f7044955d3d86812c2349e07a
Hustra03 <erik.paulinder@gmail.com>
2023-04-28 11:49:47 +0200

    fixed calculateTotalPriceAndVATfterDiscount

--- model/SaleInfo.java
+++ model/SaleInfo.java
@@ -18,31 +18,67 @@ public class SaleInfo {
 
 	private Receipt receipt;
 
-	public SaleInfo(Sale sale, Discount noDiscount) {
-		this.sale=sale;
-		List<Discount> recordedDiscounts=new ArrayList<Discount>();
-		recordedDiscounts.add(noDiscount);
-		this.recordedDiscounts=recordedDiscounts;
+	private int customerId;
+
+	public SaleInfo(Sale sale) {
+		this.sale = sale;
+		List<Discount> recordedDiscounts = new ArrayList<Discount>();
+		this.recordedDiscounts = recordedDiscounts;
+		calculateTotalPriceAndVATAfterDiscount();
 	}
 
-	public SaleInfo setDiscountAndCustomerId(Discount discountList, int customerId) {
-		return null;
+	public void setDiscountAndCustomerId(List<Discount> recordedDiscounts, int customerId) {
+		this.customerId = customerId;
+		this.recordedDiscounts = recordedDiscounts;
+		calculateTotalPriceAndVATAfterDiscount();
 	}
 
 	SaleInfo updateSaleInfoPayment(CustomerPaymentDTO customerPayment) {
 		return null;
-	 
+
 	}
 
-	int getTotalPriceAfterDiscount() {
-		return 0;
+	/*
+	 * Returns iscounted total price
+	 * 
+	 * @return discountTotalPrice represents the total price after every discount
+	 * has been applied
+	 */
+	double getTotalPriceAfterDiscount() {
+		return this.discountTotalPrice;
 	}
 
-	private SaleInfo calculateTotalPriceAndVATAfterDiscount(Sale sale, Discount recordedDiscount) {
-		return null;
+	private void calculateTotalPriceAndVATAfterDiscount() {
+		if (!recordedDiscounts.isEmpty()) {
+			List<Item> soldItems = sale.getSoldItems();
+			this.discountTotalPrice = 0;
+			this.discountTotalVAT = 0;
+			for (Item item : soldItems) {
+				double actualDiscount = 0;
+				for (Discount discount : recordedDiscounts) {
+					if (discount.getApplicableId().contains(item.getItemDescriptionDTO().getItemId())) {
+						if (actualDiscount == 0) {
+							actualDiscount += discount.getDiscountPercent() / 100;
+						}
+					} else {
+						actualDiscount *= discount.getDiscountPercent() / 100;
+					}
+				}
+				this.discountTotalPrice += item.getPriceForQuantity() * actualDiscount;
+				this.discountTotalPrice += item.getPriceForQuantity() * actualDiscount
+						* item.getItemDescriptionDTO().getVATrate() / 100;
+			}
+		}
 	}
 
-	public Sale getSale()
-	{return this.sale;}
+	{
+		discountTotalPrice = sale.getTotalPrice();
+		discountTotalVAT = sale.getTotalVAT();
+	}
+	}
+
+	public Sale getSale() {
+		return this.sale;
+	}
 
 }
