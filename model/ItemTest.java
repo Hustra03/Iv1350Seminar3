4008f62b67dd6f9601c55db2bfe5df4db74a13c9
Mi-pro <130644903+Mi-pro@users.noreply.github.com>
2023-04-28 14:23:51 +0200

    Ändrat i imports av test

--- model/ItemTest.java
+++ model/ItemTest.java
@@ -1,9 +1,9 @@
 package model;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import intergration.ItemDescriptionDTO;
 import model.Item;
