4dba1e78b578aaa41bc8ca76566a1c231750e13d
Mi-pro <130644903+Mi-pro@users.noreply.github.com>
2023-04-28 13:53:04 +0200

    Ändrat i imports av test

--- controller/ControllerTest.java
+++ controller/ControllerTest.java
@@ -1,9 +1,9 @@
 package controller;
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import intergration.DatabaseHandler;
 import model.PaymentHandler;
