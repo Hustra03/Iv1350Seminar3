bc3126d83d1371d121a85bc94d612d2c0263b242
Hustra03 <erik.paulinder@gmail.com>
2023-04-28 11:53:41 +0200

    fixed update saleInfoPayment

--- model/SaleInfo.java
+++ model/SaleInfo.java
@@ -33,8 +33,8 @@ public class SaleInfo {
 		calculateTotalPriceAndVATAfterDiscount();
 	}
 
-	SaleInfo updateSaleInfoPayment(CustomerPaymentDTO customerPayment) {
-		return null;
+	void updateSaleInfoPayment(CustomerPaymentDTO customerPayment) {
+		this.customerPayment=customerPayment;
 
 	}
 
